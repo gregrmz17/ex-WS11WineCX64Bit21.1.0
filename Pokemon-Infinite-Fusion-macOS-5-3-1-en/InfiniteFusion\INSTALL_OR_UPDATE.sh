2563ee2a329b06f106972c2c3f6939835e3d5ec0
gregrmz17 <144958114+gregrmz17@users.noreply.github.com>
2023-09-14 11:00:51 -0700

    Add files via upload

---
+++ Pokemon-Infinite-Fusion-macOS-5-3-1-en/InfiniteFusion\INSTALL_OR_UPDATE.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+./REQUIRED_BY_INSTALLER_UPDATER/7z.exe e -spf -aoa "REQUIRED_BY_INSTALLER_UPDATER/MinGit.7z"
+
+mgit="./REQUIRED_BY_INSTALLER_UPDATER/cmd/git.exe"
+$mgit init .
+$mgit remote add origin "https://github.com/infinitefusion/infinitefusion-e18.git"
+$mgit fetch origin releases
+$mgit reset --hard origin/releases
+
+./REQUIRED_BY_INSTALLER_UPDATER/7z.exe e -spf -aoa -o"Data" "Data/species.zip"
+
+echo ""
+echo "Installer/Updater made by Hungry Pickle"
+echo ""
+echo "Thank you for installing or updating!"
